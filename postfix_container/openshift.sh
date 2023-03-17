d53903c3f7f8e19660e9978bc5bf95bb654bd642
Khary Mendez <kharyam@gmail.com>
2023-03-17 17:05:05 -0400

    Escalate privilege, create cert for testing tls

---
+++ postfix_container/openshift.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+oc project postfix
+oc label --overwrite ns postfix  pod-security.kubernetes.io/enforce=privileged
+oc new-build --strategy docker --binary --name postfix
+oc start-build postfix --from-dir=. --follow
+oc create sa postfix
+oc adm policy add-scc-to-user anyuid -z postfix
+oc create -f service.yaml
+oc annotate service postfix service.beta.openshift.io/serving-cert-secret-name=postfix
+oc create configmap postfix
+oc annotate configmap postfix service.beta.openshift.io/inject-cabundle=true
+oc create -f deployment.yaml
