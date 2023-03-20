cfe95698e5efec24a4782e19d4f9e35e90d4556b
Khary Mendez <kharyam@gmail.com>
2023-03-20 16:40:35 -0400

    cleanup / testing

--- postfix_container/openshift.sh
+++ postfix_container/openshift.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+oc new-project postfix || true
 oc project postfix
-oc label --overwrite ns postfix  pod-security.kubernetes.io/enforce=privileged
+#oc label --overwrite ns postfix  pod-security.kubernetes.io/enforce=privileged
 oc new-build --strategy docker --binary --name postfix
 oc start-build postfix --from-dir=. --follow
 oc create sa postfix
 oc adm policy add-scc-to-user anyuid -z postfix
-oc create -f service.yaml
+oc create -f service-nodeport.yaml
+#oc create -f service.yaml
+#oc create route passthrough --service=postfix
 oc annotate service postfix service.beta.openshift.io/serving-cert-secret-name=postfix
 oc create configmap postfix
 oc annotate configmap postfix service.beta.openshift.io/inject-cabundle=true
